09723211665eae00ab635aea1ee01b93c01220ad
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2023-01-23 11:33:45 +0300

    Add navlink component

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { NavLink } from "react-bootstrap";
+
+function NavBar() {
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-info px-5">
+            <NavLink to='/' className="navbar-brand" >Share2Care</NavLink>
+            {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button> */}
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ms-auto">
+                    <li className="nav-item active">
+                        <NavLink to='/' className="nav-link"><h3>Home</h3> <span className="sr-only"></span></NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to='/trips' className="nav-link" ><h3>Products</h3></NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to='/signin' className="nav-link"><h3>Sign In</h3></NavLink>
+                    </li>
+                    <li className="nav-item dropdown">
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+export default NavBar;
